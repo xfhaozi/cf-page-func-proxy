654b3f15d270604e7d2d6f7079f1e39c08f4d1d4
xfhaozi <hejialing@gmail.com>
2024-12-11 22:18:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="*.nacedu.cloudns.org";
+        url.hostname="nas.nacedu.cloudns.org";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
